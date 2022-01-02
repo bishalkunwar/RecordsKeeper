0d1d612280a189602af21f99daa277502a0cede1
bishalkunwar <bishal.kunwar487@gmail.com>
2022-01-02 21:49:48 +0545

    frontend

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import AddTutorial from "./components/AddTutorial";
+import Tutorial from "./components/Tutorial";
+import TutorialList from "./components/TutorialList";
+
+function App() {
+    return ( <
+        Router >
+        <
+        nav className = "navbar navbar-expand navbar-dark bg-dark" >
+        <
+        a href = "/tutorials"
+        className = "navbar-brand" >
+        ByBishal <
+        /a>
+
+        <
+        div className = "navbar-nav mr-auto" >
+        <
+        li className = "nav-item" >
+        <
+        Link to = { "/tutorials" }
+        className = "nav-link" >
+        tutorials <
+        /Link> < /
+        li >
+
+        <
+        li className = "nav-item" >
+        <
+        Link to = { "/add" }
+        className = "nav-link" >
+        Add <
+        /Link> < /
+        li >
+
+        <
+        /div>
+
+        <
+        /nav>
+
+        <
+        div className = "container mt-3" >
+        <
+        Switch >
+        <
+        Route exact path = {
+            ["/", "/tutorials"]
+        }
+        component = { TutorialList } > < /Route> <
+        Route exact path = "/add"
+        component = { AddTutorial } > < /Route> <
+        Route path = "/tutorials/:id"
+        component = { Tutorial } > < /Route> < /
+        Switch > <
+        /div> < /
+        Router >
+    );
+};
+
+export default App;
